45f8fcc5c01b51b39fa8c743e15f1e82476fbea5
Peter Murray <pj.murray.nz@gmail.com>
2011-10-15 10:44:31 +1100

    It's currently broken but I need to eat my breakfast and my battery is dieinggit st! #hungovercoding

--- js/chiropractor.js
+++ js/chiropractor.js
@@ -2,7 +2,8 @@ var chiropractor = (function () {
 
 
   var genericHandler, defaultStatus;
-
+  var allCodes;
+  var unloading;
 
 //  var errorCallbacks = {};
 //  _(_(standardErrorHandlers).keys()).each(function (status) {
@@ -16,31 +17,76 @@ var chiropractor = (function () {
 
 
   var buildOptions = function (options) {
-    options = options ? options : {};
-      var handlerCallbacks = (options.error) ? _(options.error).defaults(genericHandler) : genericHandler;
-      options.error = function (model, resp, options) {
-        if (handlerCallbacks[resp.status] === undefined) {
-          handlerCallbacks[defaultStatus](model, resp, options);
-        } else {
-          handlerCallbacks[resp.status](model, resp, options);
+
+    if (options !== undefined) {
+      var postHook = options.error || options['post_hook'];
+      var preHook = options['pre_hook'];
+    } else {
+      options = {}
+    }
+
+    var specificHandler = {};
+
+    _(allCodes).each(function (code) {
+      specificHandler[code] = function () {
+        if (preHook) {
+          preHook();
+        }
+        options[code]();
+        if (postHook) {
+          postHook();
         }
       };
+    });
+
+    var handlerCallbacks = _(specificHandler).defaults(genericHandler);
+    options.error = function (model, resp, options) {
+      //For both no server connection and cancelled ajax request, the response will be the same. To handle this declare
+//      A function for zero status similar to:
+       var status = parseInt(resp.status);
+
+      if (handlerCallbacks[status] === undefined || (status == 0 && !unloading)) {
+        handlerCallbacks[defaultStatus](model, resp, options);
+
+      } else if (status !== 0) {
+        debugger
+        handlerCallbacks[status](model, resp, options);
+      }
+    };
+    console.log(handlerCallbacks)
     return options;
   };
 
 
   return {
-    init: function (defaults, defaultCode) {
+    init: function (defaultCode, codes, defaults) {
+      allCodes = codes;
       defaultStatus = defaultCode;
       genericHandler = defaults;
+
+      $(window).live('beforeunload', function () {
+        unloading = true;
+      })
     },
 
     use: function (model) {
+//      var view = this;
+//      for (var name in view) {
+//        if (view.hasOwnProperty(name)) {
+//          view[name] = function () {
+//            var _chiro = view.cid;
+//          }
+//        }
+//      }
+
+//      model._chiro = this.cid;
       var save = model.save;
-      model.save = function (attrs, options) {
-        var opts = buildOptions(options);
-        save.call(model,attrs, opts);
-      };
+      if (save) {
+        model.save = function (attrs, options) {
+          var opts = buildOptions(options);
+          save.call(model, attrs, opts);
+        };
+      }
       var fetch = model.fetch;
       model.fetch = function (options) {
         fetch.call(model, buildOptions(options));
--- spec/chiropractor-spec.js
+++ spec/chiropractor-spec.js
@@ -2,7 +2,7 @@ describe("Chiropractor", function () {
 
 
   describe("handler", function () {
-    var model, errorCallback;
+    var model, view, errorCallback;
 
     function serverCall(subject, status) {
       var ajaxCall = spyOn(jQuery, 'ajax');
@@ -10,22 +10,27 @@ describe("Chiropractor", function () {
       subject();
 
       var params = ajaxCall.mostRecentCall.args[0];
-      params.error(model, {status: status.toString()});
+      params.error({status: status.toString()});
     }
 
     describe("when things happen in order", function () {
       beforeEach(function () {
         errorCallback = jasmine.createSpy('500 error');
-        chiropractor.init({500: errorCallback}, 500);
+        chiropractor.init(500, [], {500: errorCallback});
         var Model = Backbone.Model.extend({
           initialize: function () {
-            chiropractor.use(this);
           },
           url: function () {
             return "test"
           }
         });
+        var View = Backbone.View.extend({
+          initialize: function () {
+            chiropractor.use(model);
+          }
+        });
         model = new Model();
+        view = new View({model: model})
       });
 
       describe("default behaviour", function () {
@@ -39,6 +44,7 @@ describe("Chiropractor", function () {
 
       describe("generic behaviour", function () {
         it("should perform the provided behaviour", function () {
+
           serverCall(function () {
             model.save();
           }, 500);
@@ -47,59 +53,110 @@ describe("Chiropractor", function () {
         });
       });
 
-
+//
       describe("specific behaviour", function () {
         var specificCallback;
         it("should override the defaults", function () {
           specificCallback = jasmine.createSpy('specific 500 error');
-
+//
           serverCall(function () {
-            model.save({}, {error: {500: specificCallback}});
+            model.save({}, {500: specificCallback});
           }, 500);
 
           expect(specificCallback).toHaveBeenCalled();
         });
       });
-    });
-    describe("when the init happens after the use call", function () {
-      it("should still work", function () {
-        errorCallback = jasmine.createSpy('500 error');
-
-        var Model = Backbone.Model.extend({
-          initialize: function () {
-            chiropractor.use(this);
-          },
-          url: function () {
-            return "test"
-          }
-        });
-        model = new Model();
-        chiropractor.init({500: errorCallback}, 500);
-        serverCall(function () {
-          model.save();
-        }, 500);
-        expect(errorCallback).toHaveBeenCalled();
-      });
-    });
+//    });
 //
+//    describe("default generic behaviour", function () {
+//      var genericCallback;
+//      it("should always get called", function () {
+//        genericCallback = jasmine.createSpy('specific 500 error');
 //
-//    describe("generic callback", function () {
-//      var otherErrorCallback;
+//        serverCall(function () {
+//          model.save({}, {error: genericCallback});
+//        }, 500);
 //
-//      beforeEach(function () {
-//        otherErrorCallback = jasmine.createSpy('unexpected error callback');
-//      });
-//      it("should get called on generic error handling", function () {
-//        REA.helpers.errorHelper.handler({}, otherErrorCallback)({}, {status: 500});
-//        expect(otherErrorCallback).toHaveBeenCalled();
-//
-//      });
-//
-//      it("should not called on specific error handling", function () {
-//        REA.helpers.errorHelper.handler({500: function () {
-//        }}, otherErrorCallback)({}, {status: 500});
-//        expect(otherErrorCallback).not.toHaveBeenCalled();
+//        expect(genericCallback).toHaveBeenCalled();
 //      });
 //    });
+//
+//    describe("when the init happens after the use call", function () {
+//      it("should still work", function () {
+//        errorCallback = jasmine.createSpy('500 error');
+//
+//        var Model = Backbone.Model.extend({
+//          initialize: function () {
+//            chiropractor.use(this);
+//          },
+//          url: function () {
+//            return "test"
+//          }
+//        });
+//        model = new Model();
+//        chiropractor.init({500: errorCallback}, 500);
+//        serverCall(function () {
+//          model.save();
+//        }, 500);
+//        expect(errorCallback).toHaveBeenCalled();
+//      });
+//    });
+//
+//    describe("it should work with collections", function () {
+//      var collection;
+//      beforeEach(function () {
+//        errorCallback = jasmine.createSpy('500 error');
+//        chiropractor.init({500: errorCallback}, 500);
+//        var Collection = Backbone.Collection.extend({
+//          initialize: function () {
+//            chiropractor.use(this);
+//          },
+//          url: function () {
+//            return "test"
+//          }
+//        });
+//        collection = new Collection();
+//      });
+//
+//      describe("default behaviour", function () {
+//
+//        it("should default to 500", function () {
+//          serverCall(function () {
+//            collection.fetch();
+//          }, 501);
+//          expect(errorCallback).toHaveBeenCalled();
+//        });
+//
+//
+//        it("should work with create", function () {
+//          serverCall(function () {
+//            collection.create();
+//          }, 501);
+//          expect(errorCallback).toHaveBeenCalled();
+//        })
+//      });
+//
+//
+//    });
+////
+////
+////    describe("generic callback", function () {
+////      var otherErrorCallback;
+////
+////      beforeEach(function () {
+////        otherErrorCallback = jasmine.createSpy('unexpected error callback');
+////      });
+////      it("should get called on generic error handling", function () {
+////        REA.helpers.errorHelper.handler({}, otherErrorCallback)({}, {status: 500});
+////        expect(otherErrorCallback).toHaveBeenCalled();
+////
+////      });
+////
+////      it("should not called on specific error handling", function () {
+////        REA.helpers.errorHelper.handler({500: function () {
+////        }}, otherErrorCallback)({}, {status: 500});
+////        expect(otherErrorCallback).not.toHaveBeenCalled();
+////      });
+    });
   });
 });
